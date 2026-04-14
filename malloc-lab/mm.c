b5f561c8a8a8dee62a5246ef25488573b1d3ca77
kda <1325680q@gmail.com>
2026-04-14 11:43:31 +0900

    HDRP, FTRP, NEXT_BLKP, PREV_BLKP 매크로 정의 및 extend_heap 함수, mm_free 함수, coalesce 함수 구현

--- malloc-lab/mm.c
+++ malloc-lab/mm.c
@@ -54,14 +54,7 @@ team_t team = {
 전처리기가 컴파일 전에 하는 작업은 단순한 텍스트 치환 뿐
 런타임 중 마주하는 NEXT_BLKP(bp)라는 짧은 글자를 복잡한 포인터 계산 수식 텍스트로 바꿔치기만 하고 끝남
 -> 런타임에 힙에서 bp 값이 바뀌면 그때그때 바뀐 bp값을 바탕으로 CPU가 수식을 계산하는 실제 연산은 정상적으로 런타임에 수행됨
-
-그럼 왜 쓸까?
-
-
-
 */
-
-
 
 /* single word (4) or double word (8) alignment */
 /* 워드(4바이트) 또는 더블 워드(8바이트) 정렬 */
@@ -72,6 +65,7 @@ team_t team = {
 
 // 매크로: #define으로 만드는 문자열 치환 규칙, 컴파일 전에 코드를 바꿔주는 치환 규칙임 
 // 함수 X, 상수 정의, 짧은 코드 표현, 조건부 컴파일 등에 쓰임 
+
 // size를 ALIGNMENT 단위로 올림해서 메모리 정렬된 크기(특정 바이트 단위 경계에 맞춘 크기)로 만드는 매크로 
 // (size) + (ALIGNMENT - 1): 필요한 크기를 정렬 경계보다 조금 크게 더함 -> 올림 
 // --- 
@@ -117,7 +111,6 @@ team_t team = {
 // 
 #define CHUNKSIZE (1 << 12)
 
-
 // x와 y 중 더 큰 값을 반환하는 매크로
 // 사용자가 malloc으로 asize만큼 할당을 요청했으나 현재 힙에 맞는 빈 블록이 없을 때 sbrk로 OS에 힙 영역 늘리기 요청
 // -> 매번 필요한 만큼만 늘릭면 시스템 호출 잦아짐 -> 성능 떨어짐
@@ -139,7 +132,7 @@ team_t team = {
 // 특정 메모리 주소p에 1워드 크기의 값 val을 직접 넣음 
 // GET과 마찬가지로 형 변환 후 역참조 해야 값을 바꿀 수 있음
 // 주로 PACK 매크로를 통해 하나로 합쳐진 값을 각 블록 헤더, 푸터에 넣을 때 사용함 
-#define PUT(p, value) (*(unsigned int *) (p) = (val)) 
+#define PUT(p, val) (*(unsigned int *) (p) = (val)) 
 
 // ---
 // OR: True 우선 연산 = 하나라도 True면 다 true
@@ -155,7 +148,7 @@ team_t team = {
 // GET(p)로 얻어온 각 블록 1워드 헤더, 푸터 값에서 비트 마스킹(&, AND 연산)을 통해 원하는 정보만 깔끔하게 분리
 // 0x1 == 맨 마지막 비트만 1, 나머진 0
 // -> 앞의 블록 크기 정보를 지우고 맨 마지막 1비트짜리 할당 정보만 남김  
-#define GET_ALLOC (GET(p) & 0x1)
+#define GET_ALLOC(p) (GET(p) & 0x1)
 
 // --- HDRP & FTRP: 메모리 주소를 워드 단위로 연산해서 원하는 주소값을 찾아내는 매크로 ---
 // char * 타입 형변환: 주소값을 1바이트 단위로 더하고 빼기 위해 형변환 
@@ -185,25 +178,111 @@ team_t team = {
 // 현재 bp에서 이전 블록의 크기만큼 빼기 -> 이전 블록의 페이로드 시작 주소로 이동 
 #define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
 
+// heap_listp 선언
+// heap_listp: 힙의 시작 부분에 만들어둔 프롤로그 블록을 항상 가리키는 포인터
+// -> 힙 영역 전체를 순회할 때, 변하지 않는 출발 지점을 나타내는 역할 
+static char *heap_listp;
+
 /*
  * mm_init - initialize the malloc package.
  * mm_init - malloc 패키지를 초기화합니다.
  */
 int mm_init(void)
 {
-    // 힙 영역 만들기
-    // 힙 생성 실패로 -1 반환하면 mm_init도 -1 반환 및 종료
-    if(sbrk(WSIZE) == -1)
+    // heap_listp 초기화
+    // -> 이 변수에 실제 힙의 시작 주소값(프롤로그 블록)을 넣는 작업
+
+    // 4 워드를 가져와서 빈 가용 리스트를 만듦 
+    // -> sbrk로 힙이 만들어졌으면 반환값으로 heap_listp 초기화 => 실제 힙의 시작 주소값(프롤로그 블록)을 넣음
+    // -> sbrk 힙 생성 실패로 (void *)-1 반환하면
+    if((heap_listp = mem_sbrk(4 * WSIZE)) == (void *)-1)
     {
+        // mm_init도 -1 반환 및 종료
         return -1;
     }
 
-    // PACK 매크로
-    // GET 매크로 
-    // PUT 매크로
+    // ---  생성된 힙의 4 워드 공간 초기화 ---
+    // 정렬 패딩
+    // 힙 시작 부분의 빈 공간
+    // 첫 번째 워드 
+    PUT(heap_listp + (0 * WSIZE), 0);
+    
+    // 프롤로그 헤더
+    // -> 프롤로그는 헤더(1워드) + 푸터(1워드)로 2 word 짜리 블록
+    // -> 할당 정보 비트를 1로 해줘야 다음에 들어오는 블록이 첫번째 블록이라는 것을 알 수 있음
+    // 두 번째 워드 
+    PUT(heap_listp + (1 * WSIZE), PACK(DSIZE, 1));
 
+    // 프롤로그 푸터
+    // -> 헤더에서 두 번째 워드를 사용했으니 세 번째 워드를 head_listp로 해줌
+    // -> PACK 하는 값은 헤더와 동일(푸터는 헤더의 복사본)
+    PUT(heap_listp + (2 * WSIZE), PACK(DSIZE, 1));
+    
+    // 페이로드는 초기화 하지 않음
+    // -> 실제 데이터를 저장할 블록이 아니라 힙 탐색을 위한 경계선을 만들어주는 단계이기 때문 
+    // => 묵시적 가용 리스트의 불변하는 형식 
+
+    // 헤더 - 페이로드 - 푸터 형식
+    // -> 초기화 단계에선 페이로드가 없기 때문에 헤더 - 푸터 형식이 되었고, 이걸 프롤로그라고 묶어서 부르는 것 
+    // => 프롤로그 블럭 = 페이로드 크기가 0인 블럭 
+    // => 실질적으로 프롤로그 푸터 위치가 페이로드의 시작 주소가 됨 
+
+    // 에필로그 
+    // 크기가 0인 할당 상태만 표시하는 특수 블록 
+    // 프롤로그 블록에서 세 번째 워드까지 사용했으니 에필로그는 네 번째 워드 
+    PUT(heap_listp + (3 * WSIZE), PACK(0, 1));
+
+    // heap_listp를 프롤로그 푸터 위치(페이로드 시작 위치 == bp)를 가리키게 함 
+    heap_listp += (2 * WSIZE);
+
+    // extend_heap 함수로 힙을 CHUNKSIZE로 확장
+    // 초기 가용 블록 생성
+    // 8 바이트 정렬 조건을 맞추기 위해 CHUNKSIZE를 WORD 크기로 
+    extend_heap(CHUNKSIZE/WSIZE);
+
+    // extend_heap(CHUNKSIZE)
+    // extend_heap 구현
     return 0;
 }
+
+static void *extend_heap(size_t words)
+{
+    // 블록 포인터
+    // 페이로드의 첫 바이트를 가리키는 포인터 
+    char *bp;
+    
+    // size_t: 메모리의 크기나 데이터의 길이를 바이트 단위로 나타낼 때 사용하는 부호 없는 정수(unsigned int) 자료형
+    size_t size;
+
+    // 8바이트 정렬을 조건 맞추기 위한 구문
+    // words: OS에게 요청할 메모리 크기의 워드 개수(byte X)
+    // words % 2: 홀수, 짝수 판별 -> 8바이트 정렬 조건 만족 = 8의 배수 = 짝수
+    // 1 워드 = 4 바이트, 2워드 = 8 바이트 ... -> 워드가 짝수면 무조건 8바이트 이상이 됨  
+    // 홀수라면(정렬 조건 만족 X): (words + 1) * WSIZE -> 워드 개수를 짝수로 하나 늘린 후 WISE 곱 -> 바이트 단위로 변환
+    // 짝수라면(정렬 조건 만족): 바로 words * WSIZE -> 바이트 단위로 변환 
+    size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
+    
+    // long을 붙이는 이유: sbrk 실패 반환 값은 그냥 숫자 -1이 아닌, 메모리 시작 주소인 포인터 (void *)-1
+    // -> 포인터 타입과 일반 정수를 비교하려면 포인터 크기를 온전히 담을 수 있는 넉넉한 크기의 부호 있는 정수 자료형 long으로 형 변환 필요
+    if((long)(bp = mem_sbrk(size)) == -1)
+    {
+        return NULL;
+    }
+
+    // 메모리가  늘어남에 따라 빈 공간에 새로운 경계선을 만들어줌
+    // => mm_init 함수와 같은 역할을 함 
+    // 프롤로그 헤더 초기화
+    PUT(HDRP(bp), PACK(size, 0));
+
+    // 프롤로그 푸터 초기화 
+    PUT(FTRP(bp), PACK(size, 0));
+   
+    // 에필로그 블록 초기화 
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
+
+    return coalesce(bp);
+}
+
 
 /*
  * mm_malloc - Allocate a block by incrementing the brk pointer.
@@ -230,6 +309,90 @@ void *mm_malloc(size_t size)
  */
 void mm_free(void *ptr)
 {
+    size_t size = GET_SIZE(HDRP(ptr));
+
+    PUT(HDRP(ptr), PACK(size, 0));
+
+    PUT(FTRP(ptr), PACK(size, 0));
+
+    coalesce(ptr);
+}
+
+// 병합 함수 => 앞뒤 블록 가용 상태를 확인하고 빈 블록끼리 병합 
+// 반환형 void X, void * 보이드 포인터 O
+// -> 어떤 타입의 데이터든 가리킬 수 있는 범용 포인터 주소 
+// -> 매개변수로 전달받은 bp(페이로드 시작 주소)를 그대로 반환
+static void *coalesce(void *bp)
+{
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    size_t size = GET_SIZE(HDRP(bp));
+
+    // case 01, 이전/다음 모두 할당된 상태라 병합 X
+    if(prev_alloc && next_alloc)
+    {
+        return bp;
+    }
+    // case 02, 이전 블록만 할당 상태 다음 블록은 가용 상태 
+    else if(prev_alloc && !next_alloc)
+    {
+        // 현재 블록 사이즈 + 다음 블록 사이즈(헤더로 접근)를 총 사이즈로 계산
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp))); 
+
+        // 현재 블록 헤더에 총 사이즈 값을 넣어서 한 블록 크기를 늘림
+        PUT(HDRP(bp), PACK(size, 0));
+        
+        // 다음 블록의 푸터(새로 병합된 블록의 푸터)에 헤더와 같은 값을 넣어서 블록 크기를 늘림 
+        // 왜 FTRP(NEXT_BLKP(bp))가 아닐까?
+        // #define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+        // -> FTRP 매크로는 고정된 거리를 이동하는 것이 아닌, 현재 블록 헤더에 적힌 크기를 읽어와서 그만큼 뒤로 점프해서 푸터 위치를 찾음
+        // -> GET_SIZE(HDRP(bp))로 위에서 커진 헤더의 사이즈 값을 읽어옴 => 현재 블록 bp에서 다음 블록 푸터까지 점프 
+        // 바로 위에서 헤더를 미리 늘려놨기 떄문에 NEXT_BLKP 연산을 하지 않아도 됨 
+        PUT(FTRP(bp), PACK(size, 0));
+    }
+    // case 03, 이전 블록은 가용 상태, 다음 블록만 할당 상태
+    else if(!prev_alloc && next_alloc)
+    {
+        // 현재 블록 사이즈 + 이전 블록 사이즈(푸터로 접근)를 총 사이즈로 계산
+        // size += GET_SIZE(FTRP(PREV_BLKP(bp)));
+        // -> 위 로직도 맞지만 교재에서 아래와 같이 사용하는 이유
+        // -> 일관성 유지 때문. 
+        // -> PREV_BLKP(bp): 이전 블록의 페이로드 시작 주소로 포인터 이동
+        // -> PREV_BLKP(bp) 이후엔 이전 블록 푸터보다 헤더가 더 가까움
+        // -> 이전 블록의 bp를 구한 뒤, 그 블록의 헤더에서 크기를 가져오는 정형화된 논리적 흐름을 따르는 것이 할당기 구현에서의 표준 패턴
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+
+        // 현재 블록의 푸터를 병합된 블록의 푸터로 보고 총 사이즈 값을 넣어서 블록 크기 늘림
+        PUT(FTRP(bp), PACK(size, 0));
+
+        // 이전 블록의 헤더를 병합된 블록의 헤더로 보고 푸터와 동일한 값을 넣어서 블록 크기 늘림
+        // -> 이전 블록 + 현재 블록 = 병합 블록으로 통합
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+
+        // bp를 이전 블록으로 옮겨줌 
+        bp = PREV_BLKP(bp);
+    }
+    // case 04, 이전/다음 블록 모두 가용 상태 
+    else if(!prev_alloc && !next_alloc)
+    {
+        // 현재 블록 사이즈 + 이전 블록 사이즈 + 다음 블록 사이즈
+        size += (GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(HDRP(NEXT_BLKP(bp))));
+
+        // 이전 블록의 헤더를 병합 블록의 헤더로 보고 총 사이즈 값을 넣어서 블록 크기 늘림
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+
+        // 다음 블록의 위치로 간 뒤, 그곳의 원래 헤더 크기를 읽어 다음 블록의 푸터를 찾음 
+        //PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+        // 위와 아래는 동일한 결과가 나옴
+        // -> 케이스 2에서의 FTRP와 동일한 점프 동작으로 인해 같은 결과가 나옴 
+        // FTRP는 증가된 헤더의 사이즈값을 기준으로 푸터를 찾기 때문에 바로 이전 블록의 bp를 넣어 계산해도 됨 
+        PUT(FTRP(PREV_BLKP(bp)), PACK(size, 0));
+
+        // bp를 이전 블록으로 옮겨줌 
+        bp = PREV_BLKP(bp);
+    }
+
+    return bp;
 }
 
 /*
